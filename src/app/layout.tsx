609a022b961dc4451121e16ed73be8fbf3cc2576
nijkor <nijkor.transport@gmail.com>
2025-09-04 22:01:04 +0300

    пофиксил кеширование

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,6 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const geistSans = Geist({
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Цитаты Джейсона Стетхема",
   description: "Получи свою ежедневную дозу мотивации от Джейсона Стетхема!",
@@ -21,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.className} antialiased`}
-      >
+      <body>
         <Analytics />
         <SpeedInsights />
         <div className="h-screen flex flex-col justify-center items-center gap-5">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Quote from "@/components/quote";
 
+export const revalidate = 0
+
 export default function Home() {
   return (
     <Quote />

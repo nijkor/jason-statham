e55a80f5b5785ca1c8b20ae31d01023661854612
nijkor <nijkor.transport@gmail.com>
2025-09-04 21:51:15 +0300

    модицифирован дизайн

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -6,20 +6,24 @@ import {
 
 import { quotes } from "@/data/quotes"
 
-import { QuoteIcon } from "lucide-react"
+import { ClockIcon, QuoteIcon } from "lucide-react"
 
 export default function Quote() {
     const quote = quotes[Math.floor(Math.random() * quotes.length)]
+    const time = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
 
     return (
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row items-end gap-5">
             <Avatar>
                 <AvatarImage src="/jason-statham.jpg" />
                 <AvatarFallback>JS</AvatarFallback>
             </Avatar>
 
             <div className="flex flex-col">
-                <div className="text-xs font-medium">Джейсон Стетхем</div>
+                <div className="text-xs flex flex-row gap-2">
+                    <span className="font-medium">Джейсон Стетхем</span>
+                    <p>{time}</p>
+                </div>
                 <p className="p-2 bg-neutral-100 rounded-lg text-md italic">{quote}</p>
             </div>
         </div>

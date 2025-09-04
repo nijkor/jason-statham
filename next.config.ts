d8b72043d53b6cd9f5a5175aaab7cc9170ba2c75
nijkor <nijkor.transport@gmail.com>
2025-09-04 21:53:07 +0300

    refactor: remove MDX dependencies and components
    - Deleted MDX-related dependencies from package.json.
    - Removed the mdx-components.tsx file which contained custom MDX components.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"]
 };
 
-const configWithMdx = createMDX({
-  extension: /\.(md|mdx)$/,
-})
-
-export default configWithMdx(nextConfig);
+export default nextConfig;
